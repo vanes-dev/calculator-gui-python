30d38f8b756d17c6ad3c4cd7f795d6c947e66f5b
Vanessa Cordeiro <vanessacordeiro@gmail.com>
2023-05-15 18:23:26 -0300

    first commit

--- hello.py
+++
@@ -1,8 +0,0 @@
-# write a function that prints hello world
-def hello():
-    print("Hello World!")   # this is the body of the function 
-
-# call the function
-hello() # this is the function call
-
-# Path: hello.py
